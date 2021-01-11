3f519f88ed3fb10314b84465f3b120b0f955f3fb
Ovidiu Lucut <Ovidiu.Lucut@yahoo.com>
2021-01-11 16:54:29 +0100

    fix mobile issues on navbar and some pages

--- api-impl/src/main/resources/static/js/navbar/navbar.js
+++ api-impl/src/main/resources/static/js/navbar/navbar.js
@@ -1,3 +1,22 @@
+$(document).ready(function () {
+    $("#navbar-menu-btn").on('click', function (event) {
+        $("#navbar-mobile-items-hidden-menu").toggleClass("navbar-mobile-items-hidden");
+        animateHiddenMenu();
+    })
+    $("#navbar-back-btn").on('click', function (event) {
+        $("#navbar-mobile-items-hidden-menu").toggleClass("navbar-mobile-items-hidden");
+    })
+});
+
+function animateHiddenMenu(){
+    let hiddenMenu = $( "#navbar-mobile-items-hidden-menu");
+    hiddenMenu.fadeTo( "slow" , 0.5, function() {
+        // Animation complete.
+    });
+    hiddenMenu.fadeTo( "slow" , 1, function() {
+        // Animation complete.
+    });
+}
 // $(document).ready(function () {
 //     $("#newPortfolio").on('click', function (event) {
 //         var xhttp = new XMLHttpRequest();
